1ec0bbbece49c8f026457a0a41b6342b0806cbaf
VeberAL <aleo83@rambler.ru>
2024-06-15 00:49:55 +0500

    добавить конфигурацию

---
+++ src/test/java/reqres/config/ApiConfig.java
@@ -0,0 +1,14 @@
+package reqres.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config.properties")
+public interface ApiConfig extends Config {
+
+    @Key("baseUri")
+    String baseApiUri();
+
+    @Key("basePath")
+    @DefaultValue("/api")
+    String basePath();
+}
